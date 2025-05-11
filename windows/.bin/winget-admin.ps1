cf8f7b88a932afdd1b23d78e4d5785db3a72d9b2
Norihiro Takachi <13210979+takachino@users.noreply.github.com>
2025-05-12 08:37:03 +0900

    Add WSL update command and winget installation scripts

---
+++ windows/.bin/winget-admin.ps1
@@ -0,0 +1,1 @@
+winget install -e --id gerardog.gsudo
---
+++ windows/.bin/winget.ps1
@@ -0,0 +1,8 @@
+gsudo winget install -e --id gerardog.gsudo
+
+echo "1"
+winget install -e --id Microsoft.Edge
+
+echo "2"
+winget install -e --id Microsoft.VCRedist.2015+.x64
+echo "3"
